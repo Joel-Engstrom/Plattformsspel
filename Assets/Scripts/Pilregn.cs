9fe62751a2dcfdd8da520555d50a492df2fe1b97
Joel <mang.hansson@gmail.com>
2019-09-23 00:49:13 +0200

    Har börjat på Level1

---
+++ Assets/Scripts/Pilregn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pilregn : MonoBehaviour
+{
+    [Header("Prefaben som ska spawnas")]
+    public GameObject Pil;
+
+    [Header("Tiden mellan varje pil")]
+    public float RespawnTime = 2;
+
+    [Header("Minimala stunden mellan varje pil")]
+    [Range(0.01f, 0.2f)]
+    public float MaxSpawnRate;
+
+    [Header("Hur snabbt antalet pilar ökar med tiden")]
+    [Range(0.0f, 0.02f)]
+    public float RespawnRateIncrease = 0.01f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnArrows());
+    }
+
+    IEnumerator SpawnArrows()
+    {
+        Instantiate(Pil, transform.position, Quaternion.Euler(0,0,-90));
+        yield return new WaitForSeconds(RespawnTime);
+        if (RespawnTime >= MaxSpawnRate)
+        {
+            RespawnTime -= RespawnRateIncrease;
+        }
+        StartCoroutine(SpawnArrows());
+    }
+}
