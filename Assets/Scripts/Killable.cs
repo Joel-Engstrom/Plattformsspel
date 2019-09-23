e920723e321f9e44dc4f0cbb5694d79039d68e2a
Joel <mang.hansson@gmail.com>
2019-09-23 03:02:05 +0200

    Big Update

---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killable : MonoBehaviour
+{
+    public GameObject DeathEffect;
+    public GameObject DeathScreen;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+        Instantiate(DeathEffect, col.transform.position, Quaternion.identity);
+        Destroy(gameObject, 1f);
+        DeathScreen.SetActive(true);
+    }
+}
