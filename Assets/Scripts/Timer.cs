f5a17323fbb5dcbb3e66db022a9d3d70687b0e0c
Joel <mang.hansson@gmail.com>
2019-09-23 05:29:19 +0200

    :)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI TimerText;
+    private float TimeCounter = 0f;
+    void Update()
+    {
+        TimeCounter += Time.deltaTime;
+        TimerText.text = TimeCounter.ToString("n1");
+    }
+}
